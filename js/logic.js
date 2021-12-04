eeb87a6f0db139f788f2915f48e38367f8d45bea
Marcus <marek17.10.2001@gmail.com>
2021-12-04 23:45:49 +0100

    Add css and js

---
+++ js/logic.js
@@ -0,0 +1,46 @@
+function showHiden() {
+    var div = document.querySelector(".faq-hidden");
+    /*div.classList.toogle('faq-hidden');*/
+    if(div.style.display ==="none"){
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
+function showHiden1() {
+    var div = document.querySelector(".faq-hidden1");
+    /*div.classList.toogle('faq-hidden');*/
+    if(div.style.display ==="none"){
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
+function showHiden2() {
+    var div = document.querySelector(".faq-hidden2");
+    /*div.classList.toogle('faq-hidden');*/
+    if(div.style.display ==="none"){
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
+function showHiden3() {
+    var div = document.querySelector(".faq-hidden3");
+    /*div.classList.toogle('faq-hidden');*/
+    if(div.style.display ==="none"){
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
+function showHiden4() {
+    var div = document.querySelector(".faq-hidden4");
+    /*div.classList.toogle('faq-hidden');*/
+    if(div.style.display ==="none"){
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
+/*class="faq-answers" */ 
